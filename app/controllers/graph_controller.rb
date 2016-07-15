8b6b841a5918ecf0b482aef82045039c38e05119
Coby Randquist <randquistcp@gmail.com>
2016-07-15 13:05:29 -0700

    now data is random

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -3,9 +3,16 @@ class GraphController < ApplicationController
   end
 
   def data
+
+    data_values = []
+    
+    (1..5).each do
+      data_values << rand(10) + 1
+    end
+    
     respond_to do |format|
       format.json {
-        render json: [1,2,3,4,5,4,3,2,1]
+        render json: data_values
       }
     end
   end
