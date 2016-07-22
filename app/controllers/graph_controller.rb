afbbd73ca0852019897ba6a2bea7046672b96121
Steve Conklin <steve@conklinhouse.com>
2016-07-22 13:43:21 -0500

    fix node colors

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -9,9 +9,9 @@ class GraphController < ApplicationController
         [
           # this is a kluge. Duration for the root node sets the diameter
           { "call": "AI4QR", "duration": 0, "mode": " ", "time": " "},
-          { "call": "N2NLY", "duration": 6, "mode": "mode: data", "time": "05/12/16 13:12" },
-          { "call": "N2NLY", "duration": 60, "mode": "mode: voice", "time": "05/12/16 14:00"},
-          { "call": "N2NLY", "duration": 30, "mode": "mode: data", "time": "06/27/16 01:13"}
+          { "call": "N2NLY", "duration": 6, "mode": "data", "time": "05/12/16 13:12" },
+          { "call": "N2NLY", "duration": 60, "mode": "voice", "time": "05/12/16 14:00"},
+          { "call": "N2NLY", "duration": 30, "mode": "data", "time": "06/27/16 01:13"}
         ],
       "qsos":
         [
@@ -28,5 +28,4 @@ class GraphController < ApplicationController
       }
     end
   end
-  
 end
